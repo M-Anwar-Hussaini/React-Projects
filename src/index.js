e001be8013d79396498f354de46bafc87d7539c9
Anwar <anwarhussaini160@gmail.com>
2023-07-18 09:52:02 +0430

    Connection reduc with react

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
 import store from './store';
-store.dispatch({ type: 'account/deposit', payload: 500 });
-console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
