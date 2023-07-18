533368cdb245aacad9315039cd247cab32e9b915
Anwar <anwarhussaini160@gmail.com>
2023-07-18 08:44:03 +0430

    Create action creators

--- src/store.js
+++ src/store.js
@@ -9,7 +9,7 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'account/deposit':
       return { ...state, balance: state.balance + action.payload };
-    case 'account/widthdraw':
+    case 'account/withdraw':
       return { ...state, balance: state.balance - action.payload };
     case 'account/requestLoan':
       if (state.loan > 0) return state;
@@ -23,7 +23,7 @@ const reducer = (state = initialState, action) => {
         ...state,
         loan: 0,
         loanPurpose: '',
-        balance: state.balance - state.loan,
+        // balance: state.balance - state.loan,
       };
     default:
       return state;
@@ -31,11 +31,35 @@ const reducer = (state = initialState, action) => {
 };
 
 const store = createStore(reducer);
-store.dispatch({ type: 'account/deposit', payload: 500 });
-store.dispatch({ type: 'account/widthdraw', payload: 100 });
-store.dispatch({
-  type: 'account/requestLoan',
-  payload: { amount: 1000, purpose: 'Buy Car' },
-});
-store.dispatch({ type: 'account/payLoan' });
+// store.dispatch({ type: 'account/deposit', payload: 500 });
+// store.dispatch({ type: 'account/widthdraw', payload: 100 });
+// store.dispatch({
+//   type: 'account/requestLoan',
+//   payload: { amount: 1000, purpose: 'Buy Car' },
+// });
+// store.dispatch({ type: 'account/payLoan' });
+// console.log(store.getState());
+
+function deposit(amount) {
+  return { type: 'account/deposit', payload: amount };
+}
+function withdraw(amount) {
+  return { type: 'account/withdraw', payload: amount };
+}
+function requestLoan(amount, purpose) {
+  return {
+    type: 'account/requestLoan',
+    payload: { amount, purpose },
+  };
+}
+function payLoan() {
+  return { type: 'account/payLoan' };
+}
+
+store.dispatch(deposit(500));
+store.dispatch(withdraw(120));
+console.log(store.getState());
+store.dispatch(requestLoan(1000, 'to buy a cheap car'));
+console.log(store.getState());
+store.dispatch(payLoan());
 console.log(store.getState());
